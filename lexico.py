cc7a25cebdc2ff6e431168c67bb900c187115bdd
Paulo Viana <paulo9mv@yahoo.com.br>
2019-03-29 19:59:16 -0400

    reserved words added

--- lexico.py
+++ lexico.py
@@ -1,1 +1,41 @@
 #Analisador lexico
+reservedWords = ['ABSOLUTE', 'ARRAY','BEGIN', 'CASE', 'CHAR',
+                 'CONST', 'DIV', 'DO' ,'DOWTO' ,'ELSE', 'END',
+                 'EXTERNAL', 'FILE', 'FOR', 'FORWARD' ,'FUNC','FUNCTION','GOTO','IF', 'IMPLEMENTATION',
+                 'IN', 'INTEGER', 'INTERFACE', 'INTERRUPT', 'LABEL', 'MAIN', 'NIL', 'NIT' ,'OF', 'PACKED', 'PROC',
+                 'PROGRAM', 'REAL', 'RECORD', 'REPEAT', 'SET', 'SHL', 'SHR', 'STRING' ,'THEN', 'TO','TYPE', 'UNIT',
+                 'UNTIL', 'USES', 'VAR', 'WHILE', 'WITH', 'XOR']
+
+print len(reservedWords)
+
+
+
+def isWordReserved(term):
+    print 'oi'
+
+def isIdentifier(term):
+    print 'oi'
+
+def isIntegerNumber(term):
+    print 'oi'
+
+def isRealNumber(term):
+    print 'oi'
+
+def isArithmeticalOperator(term):
+    print 'oi'
+
+def isRelationalOperator(term):
+    print 'oi'
+
+def isLogicalOperator(term):
+    print 'oi'
+
+def isSpecialSymbol(term):
+    print 'oi'
+
+def isAssignment(term):
+    print 'oi'
+
+def isEnd(term):
+    print 'oi'
